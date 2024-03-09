2f8094487e381c7ff9e0a4e3fdbf37ca5aaddfe2
Emeks2121 <2364347724@qq.com>
2024-03-09 18:51:32 +0800

    first commit

---
+++ test2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+string checkBrackets(const string& str) {
+    stack<int> stk;
+    char result[1000];
+    int i = 0;
+    for (char ch : str) {
+        if (ch == '(') {
+            stk.push(i);
+            result[i] = ' ';
+        }
+        else if (ch == ')') {
+            if (!stk.empty()) {
+                stk.pop();
+                result[i] = ' ';
+            }
+            else {
+                result[i] = '?';
+            }
+        }
+        else {
+            result[i] = ' ';
+        }
+        i++;
+    }
+    result[i] = '\0';
+    int k;
+    while (!stk.empty()) {
+        k = stk.top();
+        result[k] = 'x';
+        stk.pop();
+    }
+    string r(result);
+    return r;
+}
+
+int main() {
+    string testCases[] = {
+        "bge)))))))))",
+        "((IIII))))))",
+        "()()()()(uuu",
+        "))))UUUU((()"
+    };
+
+    for (const string& testCase : testCases) {
+        cout << testCase << endl;
+        cout << checkBrackets(testCase) << endl;
+    }
+    return 0;
+}
